e232455d2de0ee9fd33c4e41e493639908620101
ParseUser <maximboom6@gmail.com>
2025-06-10 00:28:44 +0300

    Добавлена отладка для ADMIN_IDS

--- config/config.py
+++ config/config.py
@@ -5,4 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SUPPORT_BOT_TOKEN = os.getenv("SUPPORT_BOT_TOKEN")
-ADMIN_IDS = os.getenv("ADMIN_IDS", "").split(",")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
+
+# Добавь отладочный вывод
+print(f"Loaded ADMIN_IDS: {ADMIN_IDS}")
