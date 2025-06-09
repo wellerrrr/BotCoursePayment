f2b2c6ee47a25c81961946f5a3daeebbeb95174c
ParseUser <maximboom6@gmail.com>
2025-06-09 18:55:12 +0300

    config update

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "default_token_if_not_set")
-SUPPORT_BOT_TOKEN = os.getenv("SUPPORT_BOT_TOKEN", "default_support_token_if_not_set")
-ADMIN_IDS = eval(os.getenv("ADMIN_IDS", "[0]"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPPORT_BOT_TOKEN = os.getenv("SUPPORT_BOT_TOKEN")
+ADMIN_IDS = eval(os.getenv("ADMIN_IDS"))
