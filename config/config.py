09542f6e6394a14bd0b71544f34c49ac1169b7d8
ParseUser <maximboom6@gmail.com>
2025-06-10 00:08:36 +0300

    Добавлена конфигурация токенов и .env

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SUPPORT_BOT_TOKEN = os.getenv("SUPPORT_BOT_TOKEN")
-ADMIN_IDS = eval(os.getenv("ADMIN_IDS"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_default_token_here")
+SUPPORT_BOT_TOKEN = os.getenv("SUPPORT_BOT_TOKEN", "your_default_support_token_here")
+ADMIN_IDS = os.getenv("ADMIN_IDS", "").split(",")
