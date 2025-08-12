653e273aeb3b3a78d714ab31d427345892c9b168
ParseUser <maximboom6@gmail.com>
2025-08-12 12:00:46 +0300

    убрал web хуки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import CommandStart
 from aiogram import Bot, Dispatcher, F
 
 from handlers import callbacks, admin, start, db_callback_messages
@@ -6,24 +7,9 @@ from services.reviews import ReviewService
 from middlewares.admin import AdminPhotoMiddleware
 
 from config import config
-from aiohttp import web
 
 import logging
 import asyncio
-
-
-async def handle_webhook(request, bot: Bot):
-    data = await request.json()
-    payment = data['object']
-    
-    if payment['status'] == 'succeeded':
-        user_id = payment['metadata']['user_id']
-        await bot.send_message(user_id, "✅ Платеж подтверждён! Доступ: t.me/ваш_канал")
-    
-    return web.Response(text="OK")
-
-app = web.Application()
-app.router.add_post('/yookassa_webhook', handle_webhook)
 
 
 async def run_bot(dispatcher: Dispatcher, bot: Bot):
@@ -63,7 +49,6 @@ async def main():
 if __name__ == '__main__':
     try:
         logger.info("Запускаем бот")
-        web.run_app(app, port=3000)
         asyncio.run(main())
     except KeyboardInterrupt as e:
         print('Бот выключен!')
